ee927cc462ffe453222508f42a535f96be327f77
Juliano Krieger <julianosk@gmail.com>
2021-06-21 14:43:04 +0200

    Root only

--- locustfile.py
+++ locustfile.py
@@ -1,11 +1,6 @@
 import base64
 
-from locust import HttpUser, TaskSet, task
-from random import randint, choice
-
-
-base64string = base64.b64encode(b'user:password')
-auth_header = "Basic %s" % base64string.decode('utf-8')
+from locust import HttpUser, task
 
 
 class SockShopUser(HttpUser):
@@ -14,14 +9,3 @@ class SockShopUser(HttpUser):
     def load(self):
 
         self.client.get("/")
-        self.client.get("/login", headers={"Authorization": auth_header})
-        catalogue = self.client.get("/catalogue").json()
-        category_item = choice(catalogue)
-        item_id = category_item["id"]
-
-        self.client.get("/category.html")
-        self.client.get("/detail.html?id={}".format(item_id))
-        self.client.delete("/cart")
-        self.client.post("/cart", json={"id": item_id, "quantity": 1})
-        self.client.get("/basket.html")
-        self.client.get("/orders")
